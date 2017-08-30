93d0a0c9a585a7a8e8e2eb746f31f18fcf6eb7c7
Daiz <taneli.vatanen@gmail.com>
2017-08-30 20:00:46 +0300

    Add documentation to math module functions

--- src/math.ts
+++ src/math.ts
@@ -1,3 +1,9 @@
+/**
+ * Linearly interpolate between two numbers.
+ * @param a The first parameter.
+ * @param b The second parameter.
+ * @param t The interpolation factor. 0.0 for a, 1.0 for b.
+ */
 export function lerp(a: number, b: number, t: number): number {
   return a + (b - a) * t;
 }
@@ -8,7 +14,12 @@ interface IBounds {
   width: number;
   height: number;
 }
-
+/**
+ * Test if a point is inside a rectangle.
+ * @param x X coordinate of the point.
+ * @param y Y coordinate of the point.
+ * @param r The rectangle bounds.
+ */
 export function inbound(x: number, y: number, r: IBounds): boolean {
   if (x < r.x || x > r.x + r.width || y < r.y || y > r.y + r.height) {
     return false;
@@ -16,7 +27,11 @@ export function inbound(x: number, y: number, r: IBounds): boolean {
     return true;
   }
 }
-
+/**
+ * Check if two rectangles overlap each other.
+ * @param r1 The first rectangle.
+ * @param r2 The second rectangle.
+ */
 export function overlap(r1: IBounds, r2: IBounds): boolean {
   const check = [
     r1.x + r1.width < r2.x,
